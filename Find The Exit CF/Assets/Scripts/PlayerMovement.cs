5528d74d7b77f51b617319322b0798a658e5bc05
fundamental-group <emathstutorials@gmail.com>
2023-01-20 14:18:53 -0800

    Cloud Hop Find the Exit Jungle Escape

---
+++ Find The Exit CF/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 destination = new Vector3(horizontal, 0);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(destination * speed * Time.deltaTime);
+    }
+}
