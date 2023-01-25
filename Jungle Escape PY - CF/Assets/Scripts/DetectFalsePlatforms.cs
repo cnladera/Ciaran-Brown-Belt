fd7ba69c82643d101362d999eb62d862698feb19
fundamental-group <emathstutorials@gmail.com>
2023-01-25 13:41:25 -0800

    Jungle Escape PY Added

---
+++ Jungle Escape PY - CF/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    public bool hit;
+    int FalseLayer = 1 << 8;
+
+    void Update()
+    {
+        hit = Physics.Raycast(transform.position, transform.forward, 3, FalseLayer);
+        Debug.DrawRay(transform.position, transform.forward * 3, Color.red);
+
+
+        if (hit)
+        {
+            Debug.LogWarning("Be Careful!");
+        }
+
+        if (!hit)
+        {
+            Debug.Log("All Clear");
+        }
+    }
+}
