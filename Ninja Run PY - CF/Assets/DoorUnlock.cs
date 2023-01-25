ab7622b38009b8bf22a6f6fe371fc3e00fd801d5
fundamental-group <emathstutorials@gmail.com>
2023-01-25 13:52:27 -0800

    Ninja Run PY Added

---
+++ Ninja Run PY - CF/Assets/DoorUnlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorUnlock : MonoBehaviour
+{
+    public GameObject door;
+    // Start is called before the first frame update
+ void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(door);
+            Destroy(other.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
