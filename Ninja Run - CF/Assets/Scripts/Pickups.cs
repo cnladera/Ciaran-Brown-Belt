85e55d6950fc6ba551909252e2ee93dfb91e5320
fundamental-group <emathstutorials@gmail.com>
2023-01-25 13:45:32 -0800

    Ninja Run Added

---
+++ Ninja Run - CF/Assets/Scripts/Pickups.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+
+    public int score;
+    public Text scoreText;
+
+    public ParticleSystem Pickup;
+
+    void Start()
+    {
+        Pickup.Stop();
+    }
+
+    void Update()
+    {
+    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            score++;
+            scoreText.text = score.ToString();
+            Destroy(other.gameObject);
+
+            Pickup.Play();
+        }
+    }
+}
