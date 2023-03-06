f5a245550fe2ef7f0c9797069a14a0629a04f64d
fundamental-group <emathstutorials@gmail.com>
2023-03-06 13:19:35 -0800

    CF Labyrinth

---
+++ Labyrinth - CF/Assets/MoveToGoal1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToGoal1 : MonoBehaviour
+{
+    public Transform goal;
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
